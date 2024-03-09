1b0d068060dbac0d450c534d1b78400ee92ce795
savoyskiy <savoyskiy83@yandex.ru>
2024-03-09 13:28:57 +0300

    добавляет декоративные элементы (продолжение)

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,12 +1,16 @@
 let navList = document.querySelector('.navigation__list');
-let navButton = document.querySelector('.navigation__button--opener');
+let navButton = document.querySelector('.navigation__button');
 
 navList.classList.remove('navigation__list--no-js');
 
 navButton.addEventListener('click', function () {
   if (navList.classList.contains('navigation__list--hidden')) {
     navList.classList.remove('navigation__list--hidden');
+    navButton.classList.remove('navigation__button--close');
+    navButton.classList.add('navigation__button--open');
   } else {
     navList.classList.add('navigation__list--hidden');
+    navButton.classList.add('navigation__button--close');
+    navButton.classList.remove('navigation__button--open');
   }
 });
